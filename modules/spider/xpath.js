0f003c05cbc9009258f246197f426f9e27cd021f
Pei-Jung Lan <pei-jung.lan@liferay.com>
2014-01-20 12:59:56 -0800

    Modified xpath.js to return short xpath instead of full explicit path

--- modules/spider/xpath.js
+++ modules/spider/xpath.js
@@ -1,3 +1,38 @@
+/*
+Software License Agreement (BSD License)
+
+Copyright (c) 2009, Mozilla Foundation
+All rights reserved.
+
+Redistribution and use of this software in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above
+  copyright notice, this list of conditions and the
+  following disclaimer.
+
+* Redistributions in binary form must reproduce the above
+  copyright notice, this list of conditions and the
+  following disclaimer in the documentation and/or other
+  materials provided with the distribution.
+
+* Neither the name of Mozilla Foundation nor the names of its
+  contributors may be used to endorse or promote products
+  derived from this software without specific prior
+  written permission of Mozilla Foundation.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Source: https://code.google.com/p/fbug/source/browse/branches/firebug1.6/content/firebug/lib.js?spec=svn12950&r=8828#1332
+*/
+
 function getSelector(element) {
 	if (element && element.id && element.id !='') {
 		return '#' + element.id;
@@ -13,6 +48,11 @@ function getXpath(element) {
 	// Use nodeName (instead of localName) so namespace prefix is included (if any).
 	for (; element && element.nodeType == 1; element = element.parentNode)
 	{
+		if (element.id) {
+			paths.splice(0, 0, '/*[@id="' + element.id + '"]');
+			break;
+		}
+
 		var index = 0;
 		for (var sibling = element.previousSibling; sibling; sibling = sibling.previousSibling)
 		{
